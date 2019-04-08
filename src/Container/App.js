7c5c435cb78ee7e10cff702cc43acc01419c12c1
mehrzad <m.h_hack@yahoo.com>
2019-04-08 14:48:52 +0430

    About component created

--- src/Container/App.js
+++ src/Container/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import About from './../Components/About';
 import Slider from "react-slick";
 
 class App extends Component {
@@ -42,14 +43,14 @@ class App extends Component {
 
                        
                         <div class="divider">
-                         
+                         {/*
                      
                             <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1930 255.5" style="enable-background:new 0 0 1930 255.5;" xml:space="preserve">
                            
                                 <style type="text/css">
                                     .st0{fill:none;stroke:#3c3c3c;stroke-width:10;stroke-linecap:square;stroke-miterlimit:10;}
                                 </style>
-                           
+                                
                                 <polyline class="st0" points="1224,171.8 1181.3,171.8 1139.2,129.6 1065,203.9 970.5,110.4 876,203.6 801.8,129.4 759.7,171.5 
                                     717,171.5 "/>
                                 <polyline class="st0" points="5,131.5 757.3,131.5 801.8,176.1 885.9,91.9 868.3,74.2 831.5,111 870.4,149.9 970.2,50.2 1070,149.9 
@@ -59,7 +60,7 @@ class App extends Component {
                                 <rect x="940.3" y="178.1" transform="matrix(0.7071 -0.7071 0.7071 0.7071 137.3893 746.1556)" class="st0" width="58.2" height="58.2"/>
                                 </svg>
                                 
-                        
+                         */}
                         </div>
                      
                     
@@ -129,6 +130,7 @@ class App extends Component {
         <div class="container">
             <h1 class="header-txt">This month specials</h1>
             <div class="divider">
+           
                 {/*
                 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1930 255.5" style="enable-background:new 0 0 1930 255.5;" xml:space="preserve">
                   
@@ -234,6 +236,7 @@ class App extends Component {
             <div class="about-content">
                 <h1 class="header-txt scroll-reveal">About us</h1>
                 <div class="divider scroll-reveal">
+                
                   {/*
                     <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1930 255.5" style="enable-background:new 0 0 1930 255.5;" xml:space="preserve">
                       
@@ -251,8 +254,8 @@ class App extends Component {
                                 </svg>
                                 */}
                 </div>
-             
-                <p class="scroll-reveal" data-origin="top" data-distance="10%">Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                <About />
+                            <p class="scroll-reveal" data-origin="top" data-distance="10%">Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
                 <img class="signature scroll-reveal" data-origin="right" data-distance="20%" src="./assets/signature.png" />
             </div>
         </div>
